47251dcc628e15551007ecf0101c169f64b91629
YouSeeImNewHere <61259227+YouSeeImNewHere@users.noreply.github.com>
2026-02-02 21:32:06 -0800

    Update app_postgres.py

--- app_postgres.py
+++ app_postgres.py
@@ -1412,7 +1412,7 @@ def bank_info():
         if has_card_benefits:
             cur.execute(
                 """
-                SELECT id AS account_id, category, cashback_percent
+                SELECT card_id AS account_id, category, cashback_percent
                 FROM card_benefits
                 ORDER BY account_id, category
                 """
